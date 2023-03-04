f7c2af2004874d048da6df0bbabd851bb7db251c
leslietu123 <126388542+leslietu123@users.noreply.github.com>
2023-03-04 15:34:18 +0000

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chat-qomy.onrender.com', {
+  const response = await fetch('https://chat.fun-code.top', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
